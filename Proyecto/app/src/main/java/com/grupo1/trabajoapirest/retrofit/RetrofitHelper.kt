b3b3db764803fc249b605bc4850682ae6fcc409e
Manuel de la Fuente <m@nueljl.in>
2022-06-02 10:16:27 +0200

    arreglar retrofit

--- Proyecto/app/src/main/java/com/grupo1/trabajoapirest/retrofit/RetrofitHelper.kt
+++ Proyecto/app/src/main/java/com/grupo1/trabajoapirest/retrofit/RetrofitHelper.kt
@@ -1,25 +1,28 @@
 package com.grupo1.trabajoapirest.retrofit
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitHelper {
 
-    val BASEURL = "https://api.themoviedb.org/3/"
+object RetrofitHelper
+{
+	val BASEURL = "https://api.themoviedb.org/3/"
 
-    var retrofitService: RetrofitService? = null
+	var retrofitService: RetrofitService? = null
 
-    fun getRotrofit(): RetrofitService {
-        if (retrofitService == null) {
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BASEURL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+	fun getRetrofit(): RetrofitService
+	{
+		if (retrofitService == null) {
+			val retrofit = Retrofit.Builder()
+				.baseUrl(BASEURL)
+				.addConverterFactory(GsonConverterFactory.create())
+				.client(OkHttpClient())
+				.build()
 
-            retrofitService = retrofit.create(RetrofitService::class.java)
-        }
+			retrofitService = retrofit.create(RetrofitService::class.java)
+		}
 
-        return retrofitService!!
-    }
-
+		return retrofitService!!
+	}
 }
