744830881e938a0fbb66eb3fe1504f3b6e00b410
Manuel de la Fuente <m@nueljl.in>
2022-06-09 10:26:02 +0200

    cambios mainactivity, comienzos jetpack nav

---
+++ Proyecto/app/src/main/java/com/grupo1/trabajoapirest/listaPelisFragment.kt
@@ -0,0 +1,23 @@
+package com.grupo1.trabajoapirest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.grupo1.trabajoapirest.databinding.FragmentPelisListaBinding
+
+
+class listaPelisFragment: Fragment()
+{
+	private lateinit var binding: FragmentPelisListaBinding
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+		binding = FragmentPelisListaBinding.inflate(inflater,	container, false)
+		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+	}
+}
