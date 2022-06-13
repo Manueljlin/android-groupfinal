4c641827445a18735504a95b104e59635c414d7c
Manuel de la Fuente <m@nueljl.in>
2022-06-13 11:56:43 +0200

    Crear de nuevo el dataclass

--- Proyecto/app/src/main/java/com/grupo1/trabajoapirest/retrofit/RetrofitService.kt
+++ Proyecto/app/src/main/java/com/grupo1/trabajoapirest/retrofit/RetrofitService.kt
@@ -1,16 +1,24 @@
 package com.grupo1.trabajoapirest.retrofit
 
 import com.grupo1.trabajoapirest.API_KEY
-import com.grupo1.trabajoapirest.dataclass2.ListaPelis2
-import okhttp3.Response
+import com.grupo1.trabajoapirest.dataclass.Movies.MovieDetails
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
 import java.util.ArrayList
 
 interface RetrofitService {
 
+    // movie
+        // get details
+        // https://developers.themoviedb.org/3/movies/get-movie-details
     @Headers("api_key:${API_KEY.MovieApiToken}")
-    @GET("movie/popular")
-    suspend fun getPelisPopulares(): Response<ArrayList<ListaPelis2>>
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetails(
+        @Path("movie_id") movie_id: Int
+    ): Response<MovieDetails>
 
+        //
 }
