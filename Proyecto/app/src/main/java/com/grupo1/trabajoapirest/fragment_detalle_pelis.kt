cdcb0cd6af2804120f98437d5b53619420229969
FranJc21 <92015154+franjc2000@users.noreply.github.com>
2022-06-14 19:40:46 +0200

    Detalles de Pelis
    Intentando mostrar el nombre en el toolbar

--- Proyecto/app/src/main/java/com/grupo1/trabajoapirest/fragment_detalle_pelis.kt
+++ Proyecto/app/src/main/java/com/grupo1/trabajoapirest/fragment_detalle_pelis.kt
@@ -5,55 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
+import com.grupo1.trabajoapirest.databinding.FragmentDetallePelisBinding
+import com.grupo1.trabajoapirest.viewmodel.PelisViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [fragment_detalle_pelis.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fragment_detalle_pelis : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentDetallePelisBinding
+    private val PelisViewModel: PelisViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalle_pelis, container, false)
+        binding = FragmentDetallePelisBinding.inflate(inflater, container, false)
+        //(requireActivity() as MainActivity).changeToolbar(binding.toolbar, true)
+         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_detalle_pelis.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_detalle_pelis().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        PelisViewModel.selectedMovie.observe(viewLifecycleOwner) { peli ->
+            (requireActivity() as MainActivity).changeToolbarTitle(peli!!.title!!)
+
+//            if (peli.image != null && peli.image.url != null) {
+//                Glide.with(this).load(peli.image.url).into(binding.imagenpeli)
+//            }
+        }
     }
 }
